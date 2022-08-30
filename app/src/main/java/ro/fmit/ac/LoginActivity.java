8e8883dcaa95973ac1d457f70c3f403999d9c557
Andreea Chira <andreea.chira00@e-uvt.ro>
2022-08-30 10:17:04 +0300

    Second

--- app/src/main/java/ro/fmit/ac/LoginActivity.java
+++ app/src/main/java/ro/fmit/ac/LoginActivity.java
@@ -2,13 +2,45 @@ package ro.fmit.ac;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class LoginActivity extends AppCompatActivity {
 
+    Button btnLogin;
+    TextInputEditText edEmail, edPasswd;
+    TextView noAcc;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnLogin=findViewById(R.id.btnLogin);
+        edEmail=findViewById(R.id.et_email);
+        edPasswd=findViewById(R.id.edEmails);
+        noAcc=findViewById(R.id.tvCreateAcc);
+
+        noAcc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this,RegisterActivity.class));
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
+
+
 }
--- app/src/main/java/ro/fmit/ac/RegisterActivity.java
+++ app/src/main/java/ro/fmit/ac/RegisterActivity.java
@@ -2,12 +2,23 @@ package ro.fmit.ac;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    Button btnSignup;
+    EditText edUser,edEmail,edPass,edCfPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        btnSignup=findViewById(R.id.btnSingUp);
+        edUser=findViewById(R.id.edUsername);
+        edEmail=findViewById(R.id.edEmail);
+        edPass=findViewById(R.id.edEmails);
+        edCfPass=findViewById(R.id.edCPassword);
     }
 }
